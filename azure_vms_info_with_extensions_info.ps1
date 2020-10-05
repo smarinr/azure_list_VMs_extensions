14de8b1de12c5edd6d227c6c3be28f500a426805
smarinric <sergiomarinrichmond@gmail.com>
2020-10-05 15:23:06 -0600

    Create azure_vms_info_with_extensions_info.ps1
    0.1 version (public)

---
+++ azure_vms_info_with_extensions_info.ps1
@@ -0,0 +1,34 @@
+﻿az login
+
+$subs = az account list
+
+foreach ($sub in $subs) { 
+
+	az account set --subscription $sub.id
+    $vms = Get-AzVM
+
+	foreach ($vm in $vms) { 
+
+		$vmInfo = "" | Select ResourceGroupName,Name,Location,VmSize,OsType,Extension,Publisher,Version
+		
+		$exts = az vm extension list --resource-group $vm.ResourceGroupName --vm-name $vm.Name --query '[].{Publisher:publisher,Version:typeHandlerVersion,Extension:virtualMachineExtensionType}' -o json | ConvertFrom-Json    
+		
+		foreach ($ext in $exts) {
+		 
+			$vmInfo.Name = $vm.Name
+			$vmInfo.ResourceGroupName = $vm.ResourceGroupName
+			$vmInfo.Location = $vm.Location
+			$vmInfo.VmSize = $vm.HardwareProfile.VmSize
+			$vmInfo.OsType = $vm.StorageProfile.OsDisk.OsType
+			$vmInfo.Extension = $ext.Extension
+			$vmInfo.Publisher = $ext.Publisher
+			$vmInfo.Version = $ext.Version
+			
+			$vmInfo
+		}
+	}
+
+}
+
+
+
